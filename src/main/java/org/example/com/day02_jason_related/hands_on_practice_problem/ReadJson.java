fee97a62a1a71f0758cdc89fc27cb66accdb3cf3
hritik211074 <0111al211074hritikkumarmaurya@gmail.com>
2025-02-20 01:23:40 +0530

    [Hriitk] Read a JSON file and print all keys and values.

--- src/main/java/org/example/com/day02_jason_related/hands_on_practice_problem/ReadJson.java
+++ src/main/java/org/example/com/day02_jason_related/hands_on_practice_problem/ReadJson.java
@@ -5,9 +5,9 @@ import org.json.JSONObject; // JSON library for parsing
 
 public class ReadJson {
     public static void main(String[] args) throws Exception {
+
         // Read the entire JSON file as a string
         String content = Files.readString(Paths.get("D:\\Training_Assessment_Week_05\\src\\main\\java\\org\\example\\com\\day02_jason\\hands_on_practice_problem\\data.json"));
-
         // Convert the string into a JSONObject and print all key-value pairs
         new JSONObject(content).toMap().forEach((k, v) -> System.out.println(k + ": " + v));
     }
