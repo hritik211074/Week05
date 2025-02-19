89c32bfbd8c8d88785536a48073e356ac4f3bda2
hritik211074 <0111al211074hritikkumarmaurya@gmail.com>
2025-02-20 00:05:08 +0530

    [Hritik] deleting the day02 folder

--- src/main/java/org/example/com/day02_jason/hands_on_practice_problem/JsonToXmlConverter.java
+++
@@ -1,4 +0,0 @@
-package org.example.com.day02_jason.hands_on_practice_problem;
-
-public class JsonToXmlConverter {
-}
--- src/main/java/org/example/com/day02_jason/practice_problems/ConvertJavaObjectIntoJson.java
+++
@@ -1,4 +0,0 @@
-package org.example.com.day02_jason.practice_problems;
-
-public class ConvertJavaObjectIntoJson {
-}
