679bd2876d377d4546fa7da4386d6c1e3f47cae7
hritik211074 <0111al211074hritikkumarmaurya@gmail.com>
2025-02-20 10:09:51 +0530

    .\CsvToJsonConverter.java

--- src/main/java/org/example/com/day02_jason_related/hands_on_practice_problem/ReadJson.java
+++ src/main/java/org/example/com/day02_jason_related/hands_on_practice_problem/ReadJson.java
@@ -7,7 +7,7 @@ public class ReadJson {
     public static void main(String[] args) throws Exception {
 
         // Read the entire JSON file as a string
-        String content = Files.readString(Paths.get("D:\\Training_Assessment_Week_05\\src\\main\\java\\org\\example\\com\\day02_jason\\hands_on_practice_problem\\data.json"));
+        String content = Files.readString(Paths.get("D:\\Training_Assessment_Week_05\\src\\main\\java\\org\\example\\com\\day02_jason_related\\hands_on_practice_problem\\data.json"));
         // Convert the string into a JSONObject and print all key-value pairs
         new JSONObject(content).toMap().forEach((k, v) -> System.out.println(k + ": " + v));
     }
